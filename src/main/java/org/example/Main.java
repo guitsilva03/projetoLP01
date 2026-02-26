c4d01f59f5cfdc24bdf1f800b6fa29ff47dab6e3
Guilherme <gsilva@ubiwhere.com>
2026-02-26 16:04:26 +0000

    teste main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-            System.out.println("Olá 2");
+            System.out.println("Olá 3");
     }
 }
