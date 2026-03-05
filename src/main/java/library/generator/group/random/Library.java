30876c3b7b5d20e4e86d9079d882443e023853b3
Guilherme <gsilva@ubiwhere.com>
2026-03-05 15:52:50 +0000

    versao gerar grupo sem validacao historico

--- src/main/java/library/generator/group/random/Library.java
+++ src/main/java/library/generator/group/random/Library.java
@@ -2,13 +2,15 @@ package library.generator.group.random;
 
 import lp.trabalho1.*;
 
+import java.lang.classfile.constantpool.IntegerEntry;
+
 public class Library {
     private IODataClass ioDataClass = new IODataClass();
 
     private GroupInfo[] groupInfo;
     private String[] groupsasString;
     private StudentInfo[] studentUC;
-    private String[] studentUCasString;
+    // private String[] studentUCasString;
 
     private String[] studentGroups;
 
@@ -28,7 +30,23 @@ public class Library {
     }
 
     public void loadStudentUCasString(String filepath){
-        studentUCasString = ioDataClass.loadStudentUCasString(filepath);
+        String[] studentUCasString = ioDataClass.loadStudentUCasString(filepath);
+        studentUC = new StudentInfo[studentUCasString.length];
+
+        for(int i = 0; i < studentUC.length; i++){
+            String[] studentRow = studentUCasString[i].split(",");
+            int studentId = Integer.parseInt(studentRow[0]);
+            studentUC[i] = new StudentInfo(studentId, studentRow[1]);
+        }
+
+        //Definir tamanho do array do GroupInfo baseado no número de alunos
+        int numGrupos = (studentUC.length % 2 == 0) ? studentUC.length/2 : studentUC.length/2 + 1;
+        groupInfo = new GroupInfo[numGrupos];
+
+    }
+
+    public StudentInfo[] getStudentUC(){
+        return studentUC;
     }
 
     public void outputGroups(String filepath){
@@ -36,7 +54,43 @@ public class Library {
     }
 
     public void generateGroups(){
+        for(int i = 0; i < groupInfo.length; i++){
+            groupInfo[i] = new GroupInfo();
+            groupInfo[i].setGroupID(i + 1);
+        }
+        boolean[] validPosition = validPosition();
 
+        for(int i = 0; i<groupInfo.length; i++){
+            int student1 = selectStudent();
+            int student2 = selectStudent();
+            while (!validPosition[student1]){
+                student1 = nextStudent(student1);
+            }
+            while (!validPosition[student2]){
+                student2 = nextStudent(student2);
+            }
+            groupInfo[i].setSt1(studentUC[student1]);
+            groupInfo[i].setSt2(studentUC[student2]);
+
+        }
     }
 
+    private int nextStudent(int currentStudent){
+        if(currentStudent == groupInfo.length - 1){
+            return 0;
+        }
+        return currentStudent + 1;
+    }
+
+    private int selectStudent(){
+        return (int) (Math.random() * studentUC.length);
+    }
+
+    public boolean[] validPosition(){
+        boolean[] validPosition = new boolean[studentUC.length];
+        for(int i = 0; i < validPosition.length; i++){
+            validPosition[i] = true;
+        }
+        return validPosition;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,15 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import library.generator.group.random.Library;
+import lp.trabalho1.StudentInfo;
 
 public class Main {
-    static void main() {
-            System.out.println("Olá 3");
+    public static void main(String[] args) {
+        Library library = new Library();
+        library.loadStudentUCasString("src/files/loadStudentUCasString.txt");
+        for(StudentInfo si : library.getStudentUC()){
+            System.out.println(si.toString());
+        }
     }
 }
